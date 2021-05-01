6fb4d51c3707933a2372da2ba684029757810eb4
Vignesh <svignesh1502@gmail.com>
2021-05-01 19:01:54 +0530

    init

--- laminar.py
+++ laminar.py
@@ -1,17 +1,58 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import math  # for erf
 
 
 def get_plot(U, a, D):
+    t = np.arange(3000)
+    get_2dplot(U, a, D, t)
+    get_3dplot(U, a, D, t)
+
+
+def get_2dplot(U, a, D, t):
     k = ((a*U*2)**2)/(192*D)  # coefficient of diffusion
 
-    t = np.arange(3000)
     L = 3.62*((k*t)**0.5)
 
     fig, ax = plt.subplots()
     ax.plot(t, L)
     ax.set_title("Laminar")
-    ax.set_xlabel("Length of the Transition Region")
-    ax.set_ylabel("Time")
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Length of the Transition Region")
     st.pyplot(fig)
+
+
+def get_3dplot(U, a, D, t):
+    k = ((a*U*2)**2)/(192*D)
+
+    x = np.arange(3000)
+    x1 = np.zeros((3000, 3000))
+    z = np.zeros((3000, 3000))
+    for i in range(3000):
+        x1[i, :] = x[i] - U*t
+
+    z = [[zvalue(x1[i, j], k, t[j]) for j in range(3000)] for i in range(3000)]
+    z = np.array(z)
+
+    # for i in range(3000):
+    #     for j in range(3000):
+    #         z[i, j] = zvalue(x1[i, j], k, t[j])
+
+    ax = plt.axes(projection='3d')
+    fig = ax.get_figure()
+    ax.plot_surface(x, t, z)
+    ax.set_title("Laminar")
+    ax.set_xlabel("Distance")
+    ax.set_ylabel("Time")
+    ax.set_zlabel("C/C0")
+    st.pyplot(fig)
+
+
+def zvalue(x, k, t):
+    # if x <= 0:
+    #     z = 1/2 + (1/2)*math.erf(0.5*x*((k*t)**(-0.5)))
+    # else:
+    z = 1/2 - (1/2)*math.erf(0.5*x*((k*t)**-0.5))
+    return z
--- turbulent.py
+++ turbulent.py
@@ -2,6 +2,18 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import root
+import math
+
+
+def get_plot(rho, mu, U, a, D):
+    t = np.arange(1500)
+    R = 2*rho*U*a/mu
+    gamma = root(f, x0=0.001, args=R)
+    b = (0.5*gamma.x)**0.5
+    K = 10.1*a*b*U
+
+    get_plot2d(rho, mu, U, a, D, t, R, b, K)
+    get_plot3d(rho, mu, U, a, D, t, R, b, K)
 
 
 def f(x, data):
@@ -9,13 +21,8 @@ def f(x, data):
     return y
 
 
-def get_plot(rho, mu, U, a, D):
-    R = 2*rho*U*a/mu
-    gamma = root(f, x0=0.001, args=R)
-    b = (0.5*gamma.x)**0.5
-    K = 10.1*a*b*U
+def get_plot2d(rho, mu, U, a, D, t, R, b, K):
 
-    t = np.arange(1500)
     L = ((437*a*b*U)*t) ** 0.5
 
     fig, ax = plt.subplots()
@@ -24,3 +31,33 @@ def get_plot(rho, mu, U, a, D):
     ax.set_xlabel("Length of the Transition Region")
     ax.set_ylabel("Time")
     st.pyplot(fig)
+
+
+def get_plot3d(rho, mu, U, a, D, t, R, b, K):
+
+    x = np.arange(-500, 1000)
+
+    x1 = np.zeros((1500, 1500))
+    z = np.zeros((1500, 1500))
+    for i in range(1500):
+        x1[i, :] = x[i] - U*t
+
+    z = [[zvalue(x1[i, j], K, t[j]) for j in range(1500)] for i in range(1500)]
+    z = np.array(z)
+    # x1 = x - U*t
+
+    # X, T = np.meshgrid(x1, t)
+    # z = z_function(X, T, K)
+
+    ax = plt.axes(projection='3d')
+    fig = ax.get_figure()
+    ax.plot_surface(x, t, z)
+    ax.set_title("Turbulent")
+    ax.set_xlabel("Distance")
+    ax.set_ylabel("Time")
+    ax.set_zlabel("C/C0")
+    st.pyplot(fig)
+
+
+def zvalue(X, K, T):
+    return 1/2 - (1/2)*math.erf(0.5*X*((K*T)**-0.5))
